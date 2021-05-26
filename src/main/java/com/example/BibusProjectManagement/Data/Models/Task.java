f74d28f71a96bd0fe86fea3bddfd5644b90df3b9
dokito <kr.m.kostadinov@gmail.com>
2021-05-26 16:43:00 +0300

    Task is not complete...

--- src/main/java/com/example/BibusProjectManagement/Data/Models/Task.java
+++ src/main/java/com/example/BibusProjectManagement/Data/Models/Task.java
@@ -3,11 +3,12 @@ package com.example.BibusProjectManagement.Data.Models;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -22,6 +23,14 @@ public class Task extends BaseEntity{
     @Column(name = "status", columnDefinition = "varchar(255) default 'Created'")
     private String status;
 
-    @OneToOne
-    private User assignedTo;
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_date")
+    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm")
+    private LocalDateTime createdDateTime;
+
+    @Column(name = "end_date")
+    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm")
+    private LocalDateTime endDateTime;
 }
